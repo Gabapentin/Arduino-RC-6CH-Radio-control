13d8ba25df3c6fc39a6d3996b63e50120c0d4a6a
Cyberman <Gabapentin@users.noreply.github.com>
2017-09-27 00:58:27 +0200

    Update Config.h

--- ArduinoTX_6CH/Config.h
+++ ArduinoTX_6CH/Config.h
@@ -5,7 +5,7 @@
 // Set library for SH1106 SPI 1.3" OLED display TESTED OK! (128 bytes buffer)
 U8G2_SH1106_128X64_NONAME_1_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-/* SPI wires cabling (Pin LCD name depend on the manufacturer)
+/* SPI wires cabling (OLED Pin name dependent on the manufacturer)
 
   OLED   Arduino
   ---------------------
